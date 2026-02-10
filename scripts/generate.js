7cbe2ed9ee9d45378fe67e2bc8f27a0f8ec986c0
Devansh Chaudhary <rex.12543@gmail.com>
2026-02-11 00:17:19 +0530

    Replaced Web Fonts Generator with Fantasticon
    - removed web font generator
    - installed fantasticon for font generation
    - updated generate.js script
    - added rules for alphabetical generation keeping things consistent with early versions
    - added css.hbs for prefix & codepoint

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,26 +1,63 @@
-const webfontsGenerator = require("webfonts-generator");
 const fs = require("fs");
+const path = require("path");
 
-const types = fs.readdirSync("icons/SVG");
-const files = types.reduce((acc, type) => {
-  const typeFiles = fs.readdirSync(`icons/SVG/${type}`);
-  return acc.concat(typeFiles.map((file) => `icons/SVG/${type}/${file}`));
-}, []);
+// Fix for Windows: fantasticon's glob patterns fail with backslash path separators.
+// path.join produces "icons\SVG\**\*.svg" but glob needs "icons/SVG/**/*.svg".
+// https://github.com/tancredi/fantasticon/issues/528
+if (process.platform === "win32") {
+  const originalJoin = path.join;
+  path.join = function (...args) {
+    return originalJoin.apply(this, args).replace(/\\/g, "/");
+  };
+}
 
-webfontsGenerator(
-  {
-    files,
-    dest: "fonts/",
-    templateOptions: {
-      classPrefix: "hn-",
-      baseSelector: ".hn",
-    },
-  },
-  function (error) {
-    if (error) {
-      console.log("Fail!", error);
-    } else {
-      console.log("Done!");
-    }
-  },
-);
+// Ensure glob returns files in sorted alphabetical order so ALL generated
+// font files (SVG, TTF, WOFF, WOFF2, EOT, CSS) have glyphs in consistent
+// alphabetical order: brands → purcats → regular → solid.
+const globModule = require("glob");
+const originalGlob = globModule.glob;
+globModule.glob = async function (...args) {
+  const results = await originalGlob.apply(this, args);
+  return results.sort();
+};
+
+// Build codepoints alphabetically (brands → purcats → regular → solid),
+// matching the original webfonts-generator order. New icons added to any
+// category will sort into place automatically on the next generation.
+const svgRoot = path.resolve("icons", "SVG");
+const codepoints = {};
+let code = 0xf101;
+
+for (const dir of fs.readdirSync(svgRoot).sort()) {
+  const dirPath = path.join(svgRoot, dir);
+  if (!fs.statSync(dirPath).isDirectory()) continue;
+
+  for (const file of fs.readdirSync(dirPath).filter((f) => f.endsWith(".svg")).sort()) {
+    codepoints[path.basename(file, ".svg")] = code++;
+  }
+}
+
+// Use CJS require so fantasticon picks up our patched glob above.
+const { generateFonts } = require("fantasticon");
+
+(async () => {
+  try {
+    await generateFonts({
+      inputDir: "./icons/SVG",
+      outputDir: "./fonts",
+      name: "iconfont",
+      fontTypes: ["eot", "woff2", "woff", "ttf", "svg"],
+      assetTypes: ["css"],
+      prefix: "hn",
+      fontHeight: 24,
+      codepoints,
+      getIconId: ({ basename }) => basename,
+      templates: {
+        css: "./scripts/css.hbs",
+      },
+    });
+    console.log("Done!");
+  } catch (error) {
+    console.log("Fail!", error);
+  }
+})();
